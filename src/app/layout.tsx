afe041e2741365500debfe6e7211ba20ba1d2717
Tanish Sharma <tanishsharma080@gmail.com>
2025-05-04 20:17:13 +0530

    added SVG icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -24,9 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <a href={"/"}>
+          <div className="flex items-center gap-1">
+            <div className="relative">
+              <Shape />
+              <BookIcon />
+            </div>
+            <span className="text-xl font-bold tracking-tight text-primary-500">
+              eBook Library
+            </span>
+          </div>
+        </a>
+      </div>
+      <div>right</div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+////////// SVG
+
+const Shape = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="45"
+    height="45"
+    viewBox="0 0 24 24"
+    fill="#FF0000"
+    stroke="#FF0000"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="lucide lucide-shape"
+  >
+    <path d="M22.54 6.42a2.78 2.78 0 0 0-1.95-2C18.88 4 12 4 12 4s-6.88 0-8.59.42a2.78 2.78 0 0 0-1.95 2A29.94 29.94 0 0 0 1 12a29.94 29.94 0 0 0 .46 5.58 2.78 2.78 0 0 0 1.95 2C5.12 20 12 20 12 20s6.88 0 8.59-.42a2.78 2.78 0 0 0 1.95-2A29.94 29.94 0 0 0 23 12a29.94 29.94 0 0 0-.46-5.58z" />
+  </svg>
+);
+
+const BookIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#fff"
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="#ce7041"
+    className="absolute left-1/2 top-1/2 h-8 w-8 -translate-x-1/2 -translate-y-1/2 transform"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+    />
+  </svg>
+);
