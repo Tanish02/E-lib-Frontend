747ac55ed53ed636c0a458088a812cbb718043e4
Tanish Sharma <tanishsharma080@gmail.com>
2025-05-05 03:15:57 +0530

    start working on dataFetching in ProductList

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Image from "next/image";
 import Banner from "./components/Banner";
+import ProductList from "./components/ProductList";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <ProductList />
     </>
   );
 }
