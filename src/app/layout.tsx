3ead4578f8206861facc7ec9f4cdf7c4cfa7ed3a
Tanish Sharma <tanishsharma080@gmail.com>
2025-05-07 18:47:49 +0530

    add Footer with Download button error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t p-5 text-center">
+      <span>@ eBook Library Back+Front-End with Cloudinery</span>
+    </footer>
+  );
+};
+
+// end code
+
+export default Footer;
