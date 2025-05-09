e2f0daea754315d98602b9ad3b79cf3d72d4c34a
Tanish Sharma <tanishsharma080@gmail.com>
2025-05-09 17:01:46 +0530

    add ( suspense ) for loading ...

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -1,18 +1,29 @@
-import { Book } from "@/types";
 import React from "react";
 import BookCard from "./BookCard";
+import { Book } from "@/types";
 
-const BookList = ({ books }: { books: Book[] }) => {
+const BookList = async () => {
+    // data fetching
+  const response = await fetch(`${process.env.BACKEND_URL}/books`);
+  if (!response.ok) {
+    throw new Error("Error occurred while fetching books");
+  }
+  const books = await response.json();
+  console.log("books", books);
+
   return (
     <div
       className="grid grid-cols-1 gap-8 md:grid-cols-3
      max-w-7xl mx-auto mb-10"
     >
-      {books.map((book) => (
+      {books.map((book: Book) => (
         <BookCard key={book._id} book={book} />
       ))}
     </div>
   );
 };
+
+
+// end code 
 
 export default BookList;
---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='mino-h-[800px]  mx-auto max-w-7xl px-5 pb-10 mb-6'>
+        Loading please wait....
+        </div>
+  )
+}
+
+export default Loading
