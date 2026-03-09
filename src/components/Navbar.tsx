c402ae9b8c9f95b63cf53a84e9caf36b384118b2
Tanish Sharma <tanishsharma080@gmail.com>
2026-03-09 06:03:57 +0530

    fix : dashboard links attempt

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,12 +16,12 @@ const Navbar = () => {
           </a>
         </div>
         <div className="flex items-center gap-3">
-          <a href="http://localhost:5173/auth/login">
+          <a href={`${process.env.NEXT_DASHBOARD_URL}/auth/login`}>
             <button className="h-10 rounded-xl bg-orange-400 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-red-500 active:bg-red-700">
               Sign in
             </button>
           </a>
-          <a href="http://localhost:5173/auth/register">
+          <a href={`${process.env.NEXT_DASHBOARD_URL}/auth/register`}>
             <button className="h-10 rounded-xl bg-orange-400 px-4 py-2 text-sm font-medium text-white transition-all hover:bg-red-500 active:bg-red-700">
               Sign up
             </button>
